dd20cf5860e55fe11d73f99206e4b78651f2ee61
Weshy Joe <josephwaweru96@gmail.com>
2024-01-12 19:25:56 +0300

    connecting WebSocket

--- polling/views.py
+++ polling/views.py
@@ -9,6 +9,15 @@ from polling.models import Poll, Choice
 
 # Create your views here.
 def index(request):
+    """
+    Render a list of available polls.
+
+    This view retrieves all available polls from the database and renders
+    the 'index.html' template, displaying the list of polls.
+
+    :param request: HTTP request object
+    :return: Rendered HTML template with poll list
+    """
     latest_polls = Poll.objects.order_by('pub_date')[:5]
     context = {
         'latest_polls': latest_polls,
@@ -18,6 +27,17 @@ def index(request):
 
 
 def detail(request, poll_id):
+    """
+    Render the details of a specific poll.
+
+    This view retrieves a specific poll from the database using its ID and
+    renders the 'detail.html' template. It also handles voting using
+    a form and displays real-time poll results using AJAX polling.
+
+    :param request: HTTP request object
+    :param poll_id: ID of the poll to display
+    :return: Rendered HTML template with poll details and real-time results
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
     total_votes = poll.choice_set.aggregate(Sum('votes'))['votes__sum']
 
@@ -43,7 +63,17 @@ def detail(request, poll_id):
 
 
 def poll_results_api(request, poll_id):
+    """
+    Fetch real-time poll results as JSON.
+
+    This view fetches the real-time poll results for a specific poll and
+    returns the results in JSON format, which can be used for AJAX polling.
+
+    :param request: HTTP request object
+    :param poll_id: ID of the poll for which results are fetched
+    :return: JSON response containing poll results
+    """
     poll = get_object_or_404(Poll, pk=poll_id)
-    total_votes = poll.choice_set.aggregate(Sum('votes'))['votes_sum']
+    total_votes = poll.choice_set.aggregate(Sum('votes'))['votes__sum']
     results = [{'choice_text': choice.choice_text, 'votes': choice.votes} for choice in poll.choice_set.all()]
     return JsonResponse({'results': results, 'total_votes': total_votes})

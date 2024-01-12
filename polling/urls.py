9adfebe7295784073180a3825162e1dacf241e50
Weshy Joe <josephwaweru96@gmail.com>
2024-01-12 12:35:21 +0300

    voting views

--- polling/urls.py
+++ polling/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path 
+from django.urls import path
+from . import views
+
+app_name = 'polling'
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('<int:poll_id>/', views.detail, name='detail'),
 ]
--- polling/views.py
+++ polling/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.db.models import Sum
+from django.shortcuts import render, get_object_or_404, redirect
+
+from polling.forms import VoteForm
+from polling.models import Poll, Choice
+
 
 # Create your views here.
+def index(request):
+    latest_polls = Poll.objects.order_by('pub_date')[:5]
+    context = {
+        'latest_polls': latest_polls,
+    }
+
+    return render(request, 'index.html', context)
+
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    total_votes = poll.choice_set.aggregate(Sum('votes'))['votes__sum']
+
+    if request.method == 'POST':
+        form = VoteForm(poll_id, request.POST)
+        if form.is_valid():
+            choice_id = form.cleaned_data['choice']
+            choice = get_object_or_404(Choice, pk=choice_id)
+            choice.votes += 1
+            choice.save()
+            messages.success(request, "Thank you for voting!")
+            return redirect('polling:detail', poll_id=poll_id)
+    else:
+        form = VoteForm(poll_id)
+
+    context = {
+        'poll': poll,
+        'total_votes': total_votes,
+        'form': form
+    }
+
+    return render(request, 'detail.html', context)
